46b25e8720807e3baeebdda0ff22ee402b4a77ac
newone123rt-gif <newone123rt@gmail.com>
2026-09-04 13:35:16 +0530

    Add config.py file

---
+++ config.py
@@ -0,0 +1,14 @@
+# Telegram API Credentials
+# https://my.telegram.org/apps se yeh values le sakte ho
+
+API_ID = "YOUR_API_ID"  # Apna API ID yahan likho
+API_HASH = "YOUR_API_HASH"  # Apna API Hash yahan likho
+PHONE_NUMBER = "+91XXXXXXXXXX"  # Apna phone number yahan likho (country code ke saath)
+
+# Source Group aur Destination Channel
+SOURCE_GROUP = "group_username_or_id"  # Jis group se members lene ho
+DESTINATION_CHANNEL = "channel_username_or_id"  # Jis channel mein add karne ho
+
+# Settings
+DELAY_BETWEEN_ADDS = 0.5  # Har member ke baad kitna delay (seconds mein)
+MAX_RETRY_ATTEMPTS = 3  # Agar fail ho to kitni baar koshish karna
